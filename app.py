5e4718464aba326d9411e3aed787f7d5e9b3e20c
ragecap3-star <ragecap3@gmail.com>
2026-09-14 17:26:33 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ st.markdown("""
         text-align: center;
     }
     .winner-display {
-        font-size: 10rem;
+        font-size: 5rem;
         font-weight: 900;
         color: #ffffff;
         text-align: center;
